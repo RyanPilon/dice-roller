7816f1d4dadc30bc9719dad9353b472999c6379f
Ryan Pilon <ryanpilon1995@gmail.com>
2021-04-12 17:36:45 -0700

    best dice generator yet

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,13 @@
 import random
 
 roll_again = "y"
+random.seed(1)
+sequence = [i for i in range(1,9)]
+
 
 while roll_again == "y":
     print("Your dice are ....")
-    print(random.randint(1, 9))
+    for dice in range(6):
+	    selection = random.choice(sequence)
+	    print(selection)
     roll_again = input("Roll the dices again? Y/N - ").lower()
